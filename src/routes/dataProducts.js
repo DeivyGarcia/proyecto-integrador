54698bd7d8a92fb5c1400ca64d4e943e65578fe7
DeivyGarcia <122950438+DeivyGarcia@users.noreply.github.com>
2023-01-18 21:17:28 -0500

    Add files via upload
    primer commit

---
+++ src/routes/dataProducts.js
@@ -0,0 +1,61 @@
+let cards = {
+   1:{  img: 'imagenes/avion.jpg',
+      name: 'Avion',
+      precio: '100',
+      description: 'loremips Exercitation esse Lorem eu do aute adipisicing.',
+      productId: 1,
+      cantidad: 0
+   },
+   2:{  img: 'imagenes/bebe jugando.jpg',
+      name: 'bebe jugando.',
+      precio: '120',
+      description: 'loremipsExercitation esse Lorem eu do aute adipisicing.',
+      productId: 2,
+      cantidad: 0 
+   },
+   3:{  img: 'imagenes/botella bebe.jpg',
+      name: 'Botella de bebe',
+      precio: '120',
+      description: 'loremipsExercitation esse Lorem eu do aute adipisicing.',
+      productId: 3,
+      cantidad: 0 
+   },
+   4:{  img: 'imagenes/cajas de regalos.jpg',
+      name: 'cajas de regalo',
+      precio: '80',
+      description: 'loremipsExercitation esse Lorem eu do aute adipisicing.',
+      productId: 4,
+      cantidad: 0 
+   },
+   5:{  img: 'imagenes/camiseta robot.jpg',
+      name: 'Camiseta de robot.',
+      precio: '110',
+      description: 'loremipsExercitation esse Lorem eu do aute adipisicing.',
+      productId: 5,
+      cantidad: 0
+   },
+   6:{  img: 'imagenes/carro f1 madera.jpg',
+      name: 'Carrito f1 de Madera',
+      precio: '110',
+      description: 'loremipsExercitation esse Lorem eu do aute adipisicing.',
+      productId: 6,
+      cantidad: 0 
+   },
+   7:{  img: 'imagenes/carro lego.jpg',
+      name: 'Carrito de Lego',
+      precio: '60',
+      description: 'loremipsExercitation esse Lorem eu do aute adipisicing.',
+      productId: 7,
+      cantidad: 0 
+   },
+   8:{  img: 'imagenes/conejo de pascua.jpg',
+      name: 'Conejo de Pascua',
+      precio: '150',
+      description: 'loremipsExercitation esse Lorem eu do aute adipisicing.',
+      productId: 8,
+      cantidad: 0 
+   }
+}
+
+//export {cards}
+module.exports = cards;
